f0a7432e4e0b55e76c1610a877925abaaa879645
Evie perren <evie.butland@gmail.com>
2024-10-25 07:53:19 +0100

    Adding more form fields to BookRoomForm

--- src/components/organisms/forms/BookRoom.tsx
+++ src/components/organisms/forms/BookRoom.tsx
@@ -1,26 +1,76 @@
 import { useState } from "react";
 
 interface UserDetail {
-  name?: string;
-  email?: string;
-  phone?: string;
-  room?: number;
+  name: string;
+  email: string;
+  phone: string;
+  room: number;
 }
 
 const BookRoomForm = () => {
   const initialModel: UserDetail = {
-    name: undefined,
+    name: "",
+    email: "",
+    phone: "",
+    room: 0,
   };
   const [model, setModel] = useState(initialModel);
+
+  // Add missing type here
+  const onUpdateField = (e) => {
+    const nextFormState = {
+      ...model,
+      [e.target.name]: e.target.value,
+    };
+    setModel(nextFormState);
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(model, null, 2));
+  };
+
   return (
     // Using a fragment to save another div (excessive in this case)
     <>
       <h2>Book Room</h2>
-      <form>
-        <label>Your name</label>
-        <input value={model.name} type="text" />
+      <form onSubmit={onSubmitForm}>
+        <label htmlFor="name">Your name</label>
+        <input
+          type="string"
+          aria-label="your name field"
+          name="name"
+          value={model.name}
+          onChange={onUpdateField}
+        />
 
-        {model.name}
+        <label htmlFor="email">Your email</label>
+        <input
+          type="string"
+          aria-label="your email field"
+          name="email"
+          value={model.email}
+          onChange={onUpdateField}
+        />
+
+        <label htmlFor="phone">Phone</label>
+        <input
+          type="string"
+          aria-label="your phone field"
+          name="phone"
+          value={model.phone}
+          onChange={onUpdateField}
+        />
+
+        <label htmlFor="room">Room</label>
+        <input
+          type="number"
+          aria-label="your room field"
+          name="room"
+          value={model.room}
+          onChange={onUpdateField}
+        />
+
         <button type="submit">Save</button>
       </form>
     </>
