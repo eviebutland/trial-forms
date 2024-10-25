fd4b5d61864c24ff91660c2918d2d5c24286a07f
Evie perren <evie.butland@gmail.com>
2024-10-25 06:56:42 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="/book">Book a stay with us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/forms/BookRoom.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+interface UserDetail {
+  name?: string;
+  email?: string;
+  phone?: string;
+  room?: number;
+}
+
+const BookRoomForm = () => {
+  const initialModel: UserDetail = {
+    name: undefined,
+  };
+  const [model, setModel] = useState(initialModel);
+  return (
+    // Using a fragment to save another div (excessive in this case)
+    <>
+      <h1>Book Room</h1>
+      <form>
+        <label>Your name</label>
+        <input value={model.name} type="text" />
+
+        {model.name}
+        <button type="submit">Save</button>
+      </form>
+    </>
+  );
+};
+
+export default BookRoomForm;
